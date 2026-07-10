202ca2b059c6a6ed2b6ed418423c7f14a464bf14
Guilherme Côto <guilhermecoto86@gmail.com>
2026-07-10 13:09:13 +0100

    caminho
    Inimigo a ir para o lugar e resolução da luz

---
+++ scripts/enemy.gd
@@ -0,0 +1,19 @@
+extends PathFollow3D
+
+@export var speed = 4.0
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print("Spawn")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	progress += speed * delta
+	if progress_ratio >= 1.0:
+		end()
+		
+func end():
+	print("end of the path")
+	queue_free()
