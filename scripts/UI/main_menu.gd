f4fe53c35693192a1f29031ebebd21a81229ec4e
Guilherme Côto <guilhermecoto86@gmail.com>
2026-08-17 13:19:40 +0100

    selection levels menu

--- scripts/UI/main_menu.gd
+++ scripts/UI/main_menu.gd
@@ -1,19 +1,27 @@
 extends Control
 
+#tracks dos elementos
 const track_fire = preload("res://assets/musics/Track1_Fur_Elise.wav")
 const track_water = preload("res://assets/musics/Track3_Prelude_C_Major.wav")
 const track_wind = preload("res://assets/musics/Track5_The_Four_Seasons.wav")
 const track_electricity = preload("res://assets/musics/Track6_Waltz_of_the_flowers.wav")
+
 const tracks = [track_fire, track_water, track_wind, track_electricity]
+
 var music_index = 0
 var audio_player: AudioStreamPlayer
+
+@onready var menu = $Menu
+@onready var level_selector = $LevelSelector
 
 func _ready() -> void:
 	audio_player = AudioStreamPlayer.new()
 	add_child(audio_player)
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/cena_1.tscn")
+	level_selector.visible = true
+	menu.visible = false
+	#get_tree().change_scene_to_file("res://scenes/cena_1.tscn")
 
 func _on_play_auto_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/cena_auto.tscn")
@@ -35,3 +43,7 @@ func _on_btn_music_wind_pressed() -> void:
 
 func _on_btn_music_electricity_pressed() -> void:
 	_play_track(3)
+
+func _on_btn_back_pressed() -> void:
+	level_selector.visible = false
+	menu.visible = true
